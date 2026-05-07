ba8ca4dfa1a64b1a1db6b05c7f2b9770d395046e
16bit-16 <lhy93380@gmail.com>
2026-05-07 12:47:22 +0900

    feat: fix login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,11 @@
 export default function Login() {
     return (
         <div className="px-40 flex items-center justify-center min-h-screen">
-            <div className="flex items-center justify-center border-purple-400 shadow-2xl border rounded-2xl h-128">
-                <div className="flex items-center justify-center w-full h-full px-24 gap-1">
+            <div className="bg-zinc-800 flex items-center justify-center border-purple-400 shadow-2xl border rounded-2xl h-128">
+                <a href="/" className="flex items-center justify-center w-full h-full px-24 gap-1">
                     <p className='text-3xl font-bold'>KS</p>
                     <p className="text-3xl font-bold bg-linear-to-b from-pink-500 to-blue-500 bg-clip-text text-transparent">Music</p>
-                </div>
+                </a>
                 <div className="bg-purple-400 w-0.5 h-3/4"></div>
                 <div className="flex flex-col items-center justify-center h-full px-24 gap-12 text-center">
                     <p className="text-2xl font-bold whitespace-nowrap">로블록스로 로그인</p>
